b0d24705432655a40376cddc9227f605003717ba
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:32:05 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -12,6 +12,29 @@ class Meal
      @@all << self 
   end 
   
+  
+  def self.get_meals_by_customer(customer)
+      @@all.select{ |meal| meal.customer == customer }
+  end
+
+  def self.get_meals_by_waiter(waiter)
+      @@all.select{ |meal| meal.waiter == waiter }
+  end
+
+  def self.get_waiters_by_customer(customer)
+      waiters = []
+      @@all.each do |meal| 
+          if meal.customer == customer
+              waiters << meal.waiter
+          end
+      end
+      waiters
+  end
+
+  
+  
+  
+  
   def self.all    # is class method that returns the contents of @@all
     @@all
   end 
