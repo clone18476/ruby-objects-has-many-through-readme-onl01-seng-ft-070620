3d46c6c8c76fe8b3594626e08cc6948c8b1d46f0
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:33:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,36 +1,35 @@
 class Waiter
-  
-  attr_accessor :name, :experience
-  
-  @@all = []
-  
-  def initialize(name, experience)    # initializes with a name and years of experience
-     @name = name 
-     @experience = experience 
-     @@all << self
-  end 
-  
-  def new_meal(customer, total, tip)
-      Meal.new(self, customer, total, tip)
-  end
+    attr_accessor :name, :experience
 
-  def meals
-      Meal.get_meals_by_waiter(self)
-  end
+    @@all = []
 
-  def best_tipper
-    meal_with_biggest_tip = nil
-      Meal.all.each do |meal|
-        if (!meal_with_biggest_tip || meal.tip > meal_with_biggest_tip.tip) 
-            meal_with_biggest_tip = meal
-          end
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.get_meals_by_waiter(self)
+    end
+
+    def best_tipper
+        meal_with_biggest_tip = nil
+        Meal.all.each do |meal|
+            if (!meal_with_biggest_tip || meal.tip > meal_with_biggest_tip.tip) 
+                meal_with_biggest_tip = meal
+             end
         end
-      meal_with_biggest_tip.customer
+        meal_with_biggest_tip.customer
     end
 
     def self.all
         @@all
     end
-
-
 end
+
+
