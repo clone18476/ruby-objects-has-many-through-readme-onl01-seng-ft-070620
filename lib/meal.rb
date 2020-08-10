84d9f51a6338f602578998b569de4ba5bf7bb82e
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:36:52 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,43 +1,36 @@
 
 class Meal
-    attr_accessor :waiter, :customer, :total, :tip
+  attr_accessor :waiter, :customer, :total, :tip
 
-    @@all = []
+  @@all = []
 
-    def initialize(waiter, customer, total, tip)
-        @waiter = waiter
-        @customer = customer
-        @total = total
-        @tip = tip
-        @@all << self
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.get_meals_by_customer(customer)
+    @@all.select{ |meal| meal.customer == customer }
+  end
+
+  def self.get_meals_by_waiter(waiter)
+    @@all.select{ |meal| meal.waiter == waiter }
+  end
+
+  def self.get_waiters_by_customer(customer)
+    waiters = []
+    @@all.each do |meal| 
+      if meal.customer == customer
+        waiters << meal.waiter
+      end
     end
+    waiters
+  end
 
-    def self.get_meals_by_customer(customer)
-        @@all.select{ |meal| meal.customer == customer }
-    end
-
-    def self.get_meals_by_waiter(waiter)
-        @@all.select{ |meal| meal.waiter == waiter }
-    end
-
-    def self.get_waiters_by_customer(customer)
-        waiters = []
-        @@all.each do |meal| 
-            if meal.customer == customer
-                waiters << meal.waiter
-            end
-        end
-        waiters
-    end
-
-    def self.all
-        @@all
-    end
-end
-  
-  
-  
-  def self.all    # is class method that returns the contents of @@all
+  def self.all
     @@all
-  end 
+  end
 end
