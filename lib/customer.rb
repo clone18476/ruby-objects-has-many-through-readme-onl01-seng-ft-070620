bddcfa10bf79dfb87bba6732decf2ec4a5a3f663
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:32:45 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,30 +1,28 @@
-class Customer 
-  
-  attr_accessor :name, :age 
-  
-  @@all = []
-  
-  
-  def initialized(name, age)    # initialized with name and age
-    @name = name
-    @age = age
-    @@all << self
-  end 
-  
-  def self.all    # is class method that returns the contents of @@all
-    @@all
-  end 
-  
-  def new_meal(waiter, total, tip)   # new_meal initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip
-    Meal.new(waiter, self, total, tip)
-  end 
-  
-  def meals      # returns an Array of Meal instances associated with this customer
-    Meal.get_meals_by_customer(self)
-  end
 
-  def waiters      # returns an Array of Waiter instances associated with this customer's meals
-    Meal.get_waiters_by_customer(self)
-  end
-  
+class Customer
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.get_meals_by_customer(self)
+    end
+
+    def waiters
+        Meal.get_waiters_by_customer(self)
+    end
+
+    def self.all
+        @@all
+    end
 end
