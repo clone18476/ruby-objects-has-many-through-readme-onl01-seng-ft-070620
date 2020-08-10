ca7fcc4b6422e51279ca38e06ffa8f3e4cd539b8
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:33:00 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,37 +1,39 @@
+
 class Meal
-  
-  attr_accessor :waiter, :customer, :total, :tip 
-  
-  @@all = []
-  
-  def initialize(waiter, customer, total, tip)    # initializes with a waiter, customer, total, and tip
-     @waiter = waiter 
-     @customer = customer
-     @total = total
-     @tip = tip
-     @@all << self 
-  end 
-  
-  
-  def self.get_meals_by_customer(customer)
-      @@all.select{ |meal| meal.customer == customer }
-  end
+    attr_accessor :waiter, :customer, :total, :tip
 
-  def self.get_meals_by_waiter(waiter)
-      @@all.select{ |meal| meal.waiter == waiter }
-  end
+    @@all = []
 
-  def self.get_waiters_by_customer(customer)
-      waiters = []
-      @@all.each do |meal| 
-          if meal.customer == customer
-              waiters << meal.waiter
-          end
-      end
-      waiters
-  end
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
 
-  
+    def self.get_meals_by_customer(customer)
+        @@all.select{ |meal| meal.customer == customer }
+    end
+
+    def self.get_meals_by_waiter(waiter)
+        @@all.select{ |meal| meal.waiter == waiter }
+    end
+
+    def self.get_waiters_by_customer(customer)
+        waiters = []
+        @@all.each do |meal| 
+            if meal.customer == customer
+                waiters << meal.waiter
+            end
+        end
+        waiters
+    end
+
+    def self.all
+        @@all
+    end
+end
   
   
   
